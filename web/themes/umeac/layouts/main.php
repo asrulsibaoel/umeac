90726992cd941ade9f615ff0625ad05ab03c2a89
asrulsibaoel <asrulsibaoel@Baoelz>
2015-08-06 06:45:04 +0700

    Merge origin/master
    Conflicts:
    	js/config.router.js

--- web/themes/umeac/layouts/main.php
+++ web/themes/umeac/layouts/main.php
@@ -30,31 +30,7 @@ if (\Yii::$app->user->isGuest) {
         <meta content="" name="author" />
         <?= Html::csrfMetaTags() ?>
         <!-- BEGIN PLUGIN CSS -->
-        <link href="<?= $this->theme->baseUrl ?>/assets/plugins/fullcalendar/fullcalendar.css" rel="stylesheet" type="text/css" media="screen"/>
-        <link href="<?= $this->theme->baseUrl ?>/assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" media="screen"/>
-        <link href="<?= $this->theme->baseUrl ?>/assets/plugins/gritter/css/jquery.gritter.css" rel="stylesheet" type="text/css"/>
-        <link href="<?= $this->theme->baseUrl ?>/assets/plugins/bootstrap-datepicker/css/datepicker.css" rel="stylesheet" type="text/css" />
-        <!--<link rel="<?= $this->theme->baseUrl ?>/stylesheet" href="assets/plugins/jquery-ricksaw-chart/css/rickshaw.css" type="text/css" media="screen" >-->
-        <link rel="<?= $this->theme->baseUrl ?>/stylesheet" href="assets/plugins/jquery-morris-chart/css/morris.css" type="text/css" media="screen">
-        <link href="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-slider/css/jquery.sidr.light.css" rel="stylesheet" type="text/css" media="screen"/>
-        <link href="<?= $this->theme->baseUrl ?>/assets/plugins/bootstrap-select2/select2.css" rel="stylesheet" type="text/css" media="screen"/>
-        <link href="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-jvectormap/css/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" media="screen"/>
-        <link href="<?= $this->theme->baseUrl ?>/assets/plugins/boostrap-checkbox/css/bootstrap-checkbox.css" rel="stylesheet" type="text/css" media="screen"/>
-        <!-- END PLUGIN CSS -->
-
-        <!-- BEGIN CORE CSS FRAMEWsORK -->
-
-        <link href="<?= $this->theme->baseUrl ?>/assets/plugins/boostrapv3/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="<?= $this->theme->baseUrl ?>/assets/plugins/boostrapv3/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
-        <link href="<?= $this->theme->baseUrl ?>/assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
-        <link href="<?= $this->theme->baseUrl ?>/assets/css/animate.min.css" rel="stylesheet" type="text/css"/>
-        <!-- END CORE CSS FRAMEWORK -->
-
-        <!-- BEGIN CSS TEMPLATE -->
-        <link href="<?= $this->theme->baseUrl ?>/assets/css/style.css" rel="stylesheet" type="text/css"/>
-        <link href="<?= $this->theme->baseUrl ?>/assets/css/responsive.css" rel="stylesheet" type="text/css"/>
-        <link href="<?= $this->theme->baseUrl ?>/assets/css/custom-icon-set.css" rel="stylesheet" type="text/css"/>
-        <!-- END CSS TEMPLATE -->
+        
         <?php $this->head() ?>
     </head>
     <!-- END HEAD -->
@@ -71,7 +47,7 @@ if (\Yii::$app->user->isGuest) {
                         <li class="dropdown"> <a id="main-menu-toggle" href="#main-menu"  class="" > <div class="iconset top-menu-toggle-white"></div> </a> </li>		 
                     </ul>
                     <!-- BEGIN LOGO -->	
-                    <a href="index.html"><img src="<?= $this->theme->baseUrl ?>/assets/img/logo.png" class="logo" alt=""  data-src="<?= $this->theme->baseUrl ?>/assets/img/logo.png" data-src-retina="<?= $this->theme->baseUrl ?>/assets/img/logo2x.png" width="106" height="21"/></a>
+                    <a href="<?= Yii::$app->homeUrl ?>"><img src="<?= Yii::$app->homeUrl ?>themes/umeac/assets/img/logo.png" class="logo" alt=""  data-src="<?= Yii::$app->homeUrl ?>themes/umeac/assets/img/logo.png" data-src-retina="<?= Yii::$app->homeUrl ?>themes/umeac/assets/img/logo2x.png" width="106" height="21"/></a>
                     <!-- END LOGO -->
                 </div>
                 <!-- END RESPONSIVE MENU TOGGLER --> 
@@ -89,7 +65,7 @@ if (\Yii::$app->user->isGuest) {
                                 <div class="iconset top-down-arrow"></div>
                             </a>
                             <div class="profile-pic"> 
-                                <img src="<?= $this->theme->baseUrl ?>/assets/img/profiles/avatar_small.jpg"  alt="" data-src="<?= $this->theme->baseUrl ?>/assets/img/profiles/avatar_small.jpg" data-src-retina="<?= $this->theme->baseUrl ?>/assets/img/profiles/avatar_small2x.jpg" width="35" height="35" /> 
+                                <img src="<?= Yii::$app->homeUrl ?>themes/umeac/assets/img/profiles/avatar_small.jpg"  alt="" data-src="<?= Yii::$app->homeUrl ?>themes/umeac/assets/img/profiles/avatar_small.jpg" data-src-retina="<?= Yii::$app->homeUrl ?>themes/umeac/assets/img/profiles/avatar_small2x.jpg" width="35" height="35" /> 
                             </div>       			
                         </div>
                         <ul class="nav quick-section ">
@@ -125,7 +101,7 @@ if (\Yii::$app->user->isGuest) {
                 <!-- BEGIN MINI-PROFILE -->
                 <div class="user-info-wrapper">	
                     <div class="profile-wrapper">
-                        <img src="<?= $this->theme->baseUrl ?>/assets/img/profiles/avatar.jpg"  alt="" data-src="<?= $this->theme->baseUrl ?>/assets/img/profiles/avatar.jpg" data-src-retina="<?= $this->theme->baseUrl ?>/assets/img/profiles/avatar2x.jpg" width="69" height="69" />
+                        <img src="<?= Yii::$app->homeUrl ?>themes/umeac/assets/img/profiles/avatar.jpg"  alt="" data-src="<?= Yii::$app->homeUrl ?>themes/umeac/assets/img/profiles/avatar.jpg" data-src-retina="<?= Yii::$app->homeUrl ?>themes/umeac/assets/img/profiles/avatar2x.jpg" width="69" height="69" />
                     </div>
                     <div class="user-info">
                         <div class="greeting">Welcome</div>
@@ -158,25 +134,6 @@ if (\Yii::$app->user->isGuest) {
                     </li>
                     
                 </ul>
-                <?php
-                    echo Menu::widget([
-                        'items' => [
-                            ['label' => '<i class="icon-custom-portlets"></i><span class="title">Home</span>', 'url' => ['site/index']],
-                            ['label' => '<i class="icon-custom-portlets"></i><span class="title">About</span>', 'url' => ['site/about']],
-                            ['label' => '<i class="icon-custom-portlets"></i><span class="title">Products</span>',
-                                'url' => ['product/index'],
-                                'options' => ['class' => 'dropdown'],
-                                'template' => '<a href="{url}">{label}</a>',
-                                'items' => [
-                                    ['label' => 'New Arrivals', 'url' => ['product/new']],
-                                    ['label' => 'Most Popular', 'url' => ['product/popular']],
-                                ]
-                            ],
-                        ],
-//                        'linkTemplate' => '<i class="icon-custom-portlets"></i> <span class="title">{label}',
-                        'submenuTemplate' => "\n<ul class='sub-menu'>\n{items}\n</ul>\n",
-                    ]);
-                    ?>
                 <!-- END SIDEBAR MENU --> 
             </div>
             <!-- END SIDEBAR --> 
@@ -202,40 +159,6 @@ if (\Yii::$app->user->isGuest) {
             <!-- END CONTAINER --> 
         </div>
         <!-- BEGIN CORE JS FRAMEWORK--> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-1.8.3.min.js" type="text/javascript"></script> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-ui/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/boostrapv3/js/bootstrap.min.js" type="text/javascript"></script> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/breakpoints.js" type="text/javascript"></script> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script> 
-        <!-- END CORE JS FRAMEWORK --> 
-
-        <!-- BEGIN PAGE LEVEL JS --> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/pace/pace.min.js" type="text/javascript"></script>  
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-numberAnimate/jquery.animateNumbers.js" type="text/javascript"></script> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js" type="text/javascript"></script>  
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-block-ui/jqueryblockui.js" type="text/javascript"></script> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/bootstrap-select2/select2.min.js" type="text/javascript"></script>
-<!--        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-ricksaw-chart/js/raphael-min.js"></script> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-ricksaw-chart/js/d3.v2.js"></script>
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-ricksaw-chart/js/rickshaw.min.js"></script>-->
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-morris-chart/js/morris.min.js"></script>
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-easy-pie-chart/js/jquery.easypiechart.min.js"></script>
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-slider/jquery.sidr.min.js" type="text/javascript"></script> 	
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-jvectormap/js/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script> 	
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-jvectormap/js/jquery-jvectormap-us-lcc-en.js" type="text/javascript"></script> 	
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-sparkline/jquery-sparkline.js"></script>
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-flot/jquery.flot.min.js"></script>
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/jquery-flot/jquery.flot.animator.min.js"></script>
-        <script src="<?= $this->theme->baseUrl ?>/assets/plugins/skycons/skycons.js"></script>
-        <!-- END PAGE LEVEL PLUGINS   --> 	
-        <!-- PAGE JS --> 	
-        <script src="<?= $this->theme->baseUrl ?>/assets/js/dashboard.js" type="text/javascript"></script>
-        <!-- BEGIN CORE TEMPLATE JS --> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/js/core.js" type="text/javascript"></script> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/js/chat.js" type="text/javascript"></script> 
-        <script src="<?= $this->theme->baseUrl ?>/assets/js/demo.js" type="text/javascript"></script> 
         <!-- END CORE TEMPLATE JS -->
         <?php $this->endBody() ?>
     </body>
